dce5abcf5c8df9e426eb60fdc55bb95700a34557
tingjuch <ctjchem@gmail.com>
2021-07-28 10:25:34 +0800

    Add in-order traverse in BST

--- codes/binary_search_tree.py
+++ codes/binary_search_tree.py
@@ -36,8 +36,11 @@ class BinarySearchTree(object):
     def traverse(self, mode):
         if mode == "level":
             res = self._traverse_level_order()
+        elif mode == "in":
+            res = []
+            self._traverse_in_order(res, self.root)
         else:
-            raise NotImplementedError()
+            raise NotImplementedError(f"The '{mode}' mode is not implemented yet.")
         return res
 
     def _traverse_level_order(self):
@@ -57,13 +60,13 @@ class BinarySearchTree(object):
 
         return res
 
-    def _traverse_inorder(self, res, curr):
+    def _traverse_in_order(self, res, curr):
         if curr is None:
             return
 
         res.append(curr.val)
-        self._traverse_inorder(res, curr.left)
-        self._traverse_inorder(res, curr.right)
+        self._traverse_in_order(res, curr.left)
+        self._traverse_in_order(res, curr.right)
 
 
 class BinarySearchTreeTests(unittest.TestCase):
@@ -113,6 +116,22 @@ class BinarySearchTreeTests(unittest.TestCase):
         for n in nums:
             bst.insert(n)
         self.assertListEqual(bst.traverse(mode="level"), [10, 5, 20, 7, 25])
+
+    def test_in_order_traversal_empty_tree(self):
+        bst = BinarySearchTree()
+        self.assertListEqual(bst.traverse(mode="in"), [])
+
+    def test_in_order_traversal_one_node(self):
+        bst = BinarySearchTree()
+        bst.insert(10)
+        self.assertListEqual(bst.traverse(mode="in"), [10])
+
+    def test_in_order_traversal_multiple_nodes(self):
+        bst = BinarySearchTree()
+        nums = [10, 20, 5, 7, 25]
+        for n in nums:
+            bst.insert(n)
+        self.assertListEqual(bst.traverse(mode="in"), [10, 5, 7, 20, 25])
 
 
 if __name__ == '__main__':
